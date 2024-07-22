e8dc8e5a4d43bdbde0e10cc2986cd6fdd8ffb596
Joe Fredette <jfredett@gmail.com>
2024-07-21 22:39:02 -0400

    update dns module

--- services/dns/default.nix
+++ services/dns/default.nix
@@ -1,35 +1,54 @@
 { config, pkgs, lib, ... }: with lib; {
   options.laurelin.services.dns = {
     enable = mkEnableOption "Enable DNS service";
+
     dns = mkOption {
       type = types.any;
-      default = {};
       description = ''
-      A map of DNS records suitable for use in `blocky`'s `customDNS` configuration.
+        The blocky config for the `customDNS` section
       '';
     };
+
     domains = mkOption {
       type = types.any;
       default = {};
       description = ''
-      A map of domain names to conditionally map internally.
+        A map of domain names to conditionally map internally. This is exactly the content of
+        `conditional.mapping` in the blocky configuration.
       '';
     };
+
     upstreams = mkOption {
       type = types.listOf types.string;
-      default = [];
+      default = [
+        "1.1.1.1"
+        "1.4.4.1"
+      ];
       description = ''
-      A map of upstream DNS servers.
+        A map of upstream DNS servers. Defaults to cloudflare
+      '';
+    };
+
+    interface = mkOption {
+      type = nullOr types.string;
+      default = null;
+      description = ''
+        The network interface to bind to. If null, binds on all interfaces.
       '';
     };
   };
 
-  config = let 
+  config = let
     cfg = options.laurelin.services.dns;
+    specifiedInterface = cfg.interface != null;
   in mkIf cfg.enable {
 
-    networking.firewall.allowedTCPPorts = [ 53 853 4000 ];
-    networking.firewall.allowedUDPPorts = [ 53 853 4000 ];
+    networking.firewall.allowedTCPPorts = mkIf !specifiedInterface [ 53 853 4000 ];
+    networking.firewall.allowedUDPPorts = mkIf !specifiedInterface [ 53 853 4000 ];
+
+    networking.firewall.${cfg.interface}.allowedTCPPorts = mkIf specifiedInterface [ 53 853 4000 ];
+    networking.firewall.${cfg.interface}.allowedUDPPorts = mkIf specifiedInterface [ 53 853 4000 ];
+    
 
     environment.systemPackages = [
       pkgs.blocky
@@ -57,3 +76,32 @@
     };
   };
 }
+
+/*
+# FIXME: This was all in the parent machine's config, but it should be contained in here.
+
+
+  # TODO: Move this somehow into the dns module.
+  laurelin.blocky.settings = {
+    ports = let mkBlockyIPs = port: map (ip: "${ip}:${toString port}") [ "10.255.0.3" ]; in {
+      dns = mkBlockyIPs 53;
+      tls = mkBlockyIPs 853;
+      http = mkBlockyIPs 4000;
+      https = mkBlockyIPs 443;
+    };
+  };
+
+  # TODO: Move blocky ports into the service definition, then pull it from there
+  networking.nftables.enable = true;
+  networking.firewall = let blockyPorts = [ 53 443 853 4000 ]; in {
+    # FIXME: this is probably wrong.
+    enable = false;
+    allowedTCPPorts = blockyPorts;
+    extraInputRules = ''
+     # FIXME: I hate this, I wish it were more nix and less string.
+
+     ip saddr 192.168.1.0/24 accept comment "Allow all traffic from KANSAS (192.168.1.0/24)"
+     ip saddr 10.255.0.0/16  accept comment "Allow all traffic from CONDORCET (10.255.0.0/16)"
+    '';
+  };
+*/
