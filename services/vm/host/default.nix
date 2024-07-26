e6af04208e2c8dbbddd65457286c8ccb85966022
Joe Fredette <jfredett@gmail.com>
2024-07-26 00:19:36 -0400

    import vm-host module

---
+++ services/vm/host/default.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, nixvirt, ... }: let
+  cfg = config.laurelin.services.vm-host;
+in {
+
+  options.laurelin.services.vm-host = {
+    enable = lib.mkEnableOption "Enable VM Hosting on this machine";
+
+    backup_path = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/libvirtd/backups";
+      description = "Path to backup the libvirt config to";
+    };
+
+    bridge_name = lib.mkOption {
+      type = lib.types.str;
+      default = "dmz";
+      description = "Name of the bridge to use for VM networking";
+    };
+
+    loadout = lib.mkOption {
+      type = lib.types.anything;
+      default = {};
+      description = "The content to provide to `connections.\"qemu:///system\"`";
+    };
+  };
+
+  lib.laurelin.vm.mkLinuxVMfromTemplate = opts: domain.writeXML (domain.templates.linux opts);
+  lib.laurelin.vm.loadFromFile = path: { definition = path; active = true; };
+
+  config = lib.mkIf cfg.enable {
+    # `libvirt` Config
+    security.polkit.enable = true;
+    security.polkit.debug = true;
+
+    virtualisation.libvirtd = {
+        enable = true;
+        allowedBridges = [
+          "virbr1"
+          cfg.bridge_name
+        ];
+    };
+
+    ## NixVirt setup
+
+    virtualisation.libvirt = with nixvirt.lib; {
+      enable = true;
+      connections."qemu:///system" = config.laurelin.services.vm-host.loadout;
+    };
+  };
+};
