424b10f50e2d4cc751bd2c901021c55c9411c194
Joe Fredette <jfredett@gmail.com>
2024-07-26 18:32:51 -0400

    add users options to virtualbox module

--- services/virtualbox/default.nix
+++ services/virtualbox/default.nix
@@ -1,12 +1,18 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: with lib; {
   options.laurelin.services.virtualbox = {
-    enable = lib.mkEnableOption "Enable VirtualBox";
+    enable = mkEnableOption "Enable VirtualBox";
+    users = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Users to add to the vboxusers group";
+    };
+
   };
 
   config = lib.mkIf config.laurelin.services.virtualbox.enable {
     virtualisation.virtualbox.host.enable = true;
     virtualisation.virtualbox.host.enableExtensionPack = true;
 
-    users.extraGroups.vboxusers.members = [ "jfredett" ];
+    users.extraGroups.vboxusers.members = config.laurelin.services.virtualbox.users;
   };
 }
