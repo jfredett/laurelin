ad6f9586c7607016a9de465bf88230a9d28141cb
Joe Fredette <jfredett@gmail.com>
2024-08-08 21:18:31 -0400

    add more utility functions, re-export nixvirt

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,9 @@
 { nixvirt, ... }: let
 in {
   vm = with nixvirt.lib; {
+    inherit pool network volume domain;
     mkLinuxVMfromTemplate = opts: domain.writeXML (domain.templates.linux opts);
     loadFromFile = path: { definition = path; active = true; };
   };
+  util.fold = { join, start, vals }: builtins.foldl' (acc: v: join acc v) start vals;
 }
