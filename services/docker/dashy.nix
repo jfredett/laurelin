0bb0df49e2149f39fdc15a41524efde9cf2091bd
Joe Fredette <jfredett@gmail.com>
2024-08-18 10:22:52 -0400

    make dashy config declarative

--- services/docker/dashy.nix
+++ services/docker/dashy.nix
@@ -1,10 +1,20 @@
 { config, lib, pkgs, ... }: with lib; {
   options.laurelin.services.docker.dashy = {
     enable = mkEnableOption "Enable dashy";
+    conf = mkOption {
+      type = types.str;
+      description = ''
+        Literal YAML configuration for dashy
+      '';
+    };
   };
 
   config = let
     cfg = config.laurelin.services.docker;
+    dashyCfg = pkgs.writeTextFile {
+      name = "conf.yml";
+      text = cfg.dashy.conf;
+    };
   in mkIf cfg.jellyfin.enable {
     laurelin.services = {
       reverse-proxy = {
@@ -15,19 +25,14 @@
       };
     };
 
-    # This is wrong, I need to parse cfg.volumes for this.
-    systemd.tmpfiles.rules = [
-      "d ${cfg.host.volumeRoot}/dashy 0755 root users -"
-    ];
-
     virtualisation.oci-containers.containers = {
-        dashy = {
-          image = "lissy93/dashy";
-          ports = [ "8001:8080" ];
-          volumes = [
-            "${cfg.host.volumeRoot}/dashy:/dashy:rw"
-          ];
-        };
+      dashy = {
+        image = "lissy93/dashy";
+        ports = [ "8001:8080" ];
+        volumes = [
+          "${dashyCfg}:/app/user-data/conf.yml:ro"
+        ];
+      };
     };
   };
 }
