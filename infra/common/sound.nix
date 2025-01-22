eed052e443935ea8822e09886d0b541a6b812e4e
Joe Fredette <jfredett@gmail.com>
2025-01-22 13:46:14 -0500

    fix deprecated sound config

--- infra/common/sound.nix
+++ infra/common/sound.nix
@@ -2,13 +2,15 @@
   options.laurelin.infra.sound.enable = lib.mkEnableOption "Enable sound support";
 
   config = lib.mkIf config.laurelin.infra.sound.enable {
-    hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
+    services = {
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
     };
   };
 }
