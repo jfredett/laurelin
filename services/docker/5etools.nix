dff2a27162fd0d471e6add169c372940fe508f0b
Joe Fredette <jfredett@gmail.com>
2025-06-20 15:58:26 -0400

    5etools started
    does not work very well atm. entirely my fault, i think i should probably serve this without docker, tbh.

--- services/docker/5etools.nix
+++ services/docker/5etools.nix
@@ -13,25 +13,56 @@
   config = let
     cfg = config.laurelin.services.docker;
   in mkIf cfg.fivey-tools.enable {
-    laurelin.services = {
-      reverse-proxy = {
-        domain = "emerald.city";
-        services = {
-          "5e" = { port = 20000; };
+      laurelin.services = {
+        reverse-proxy = {
+          domain = "emerald.city";
+          services = {
+            "5e" = { port = 20000; };
+          };
+        };
+      };
+
+      systemd = {
+        tmpfiles.rules = [
+          "d ${cfg.fivey-tools.confRoot} 0755 root users -"
+        ];
+
+        services.fivey-tools-updater = {
+          description = "5e.tools content updater";
+          after = [ "podman-fivey-tools.service" ];
+          requires = [ "podman-fivey-tools.service" ];
+          path = [ pkgs.git ];
+          serviceConfig = {
+            Type = "simple";
+            RemainAfterExit = "yes";
+            ExecStart = with pkgs; pkgs.writeShellScript "5e-tools-updater.sh" /* bash */ ''
+              set -x
+              rm -rf ${cfg.fivey-tools.confRoot}/core
+              git clone "https://github.com/5etools-mirror-3/5etools-src.git" "${cfg.fivey-tools.confRoot}/core"
+              git clone "https://github.com/5etools-mirror-3/5etools-img.git" "${cfg.fivey-tools.confRoot}/core/img"
+            '';
+          };
+        };
+
+        timers.fivey-tools-updater = {
+          enable = true;
+          wantedBy = [ "network.target" ];
+          partOf = [ "fivey-tools-updater.service" ];
+          description = "Update Minder for 5e.tools";
+          timerConfig.OnCalendar = "Sun *-*-* 00:00:00"; # update every week at midnight on sunday
+          timerConfig.RandomizedDelaySec = "0s";
+        };
+      };
+
+      virtualisation.oci-containers.containers = {
+        fivey-tools = {
+          image = "typhonragewind/5etools:img-1.181.8";
+          ports = [ "7775:7775" ];
+          volumes = [
+            #"${cfg.fivey-tools.confRoot}/homebrew:/data/homebrew"
+            "${cfg.fivey-tools.confRoot}/core:/data"
+          ];
         };
       };
     };
-
-    systemd.tmpfiles.rules = [
-      "d ${cfg.host.confRoot}/homebrew 0755 root users -"
-    ];
-
-    virtualisation.oci-containers.containers = {
-      fivey-tools = {
-        image = "typhonragewind/5etools:img";
-        ports = [ "7775:7775" ];
-        volumes = [ "${cfg.fivey-tools.confRoot}/homebrew:/data/homebrew" ];
-      };
-    };
-  };
 }
