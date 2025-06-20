78b97f9b19038600d89b3f6957438ad7a6d733bb
Joe Fredette <jfredett@gmail.com>
2025-06-20 14:34:08 -0400

    foundryvtt container config

--- services/docker/foundry.nix
+++ services/docker/foundry.nix
@@ -7,6 +7,12 @@
         Where the configuration is stored
       '';
     };
+    version = mkOption {
+      type = types.str;
+      description = ''
+        Which version of foundry to run
+      '';
+    };
 
   };
 
@@ -17,16 +23,22 @@
       reverse-proxy = {
         domain = "emerald.city";
         services = {
-          foundry = { port = 50002; };
+          foundry = { port = 30000; };
         };
       };
     };
 
-    # virtualisation.oci-containers.containers = {
-    #   foundry = {
-    #     image = "foundry:8.0.2";
-    #     restart = "always";
-    #   };
-    # };
+    virtualisation.oci-containers.containers = {
+      foundry = {
+        image = "felddy/foundryvtt:${cfg.foundry.version}";
+        volumes = [
+            "${cfg.foundry.confRoot}/data:/data"
+        ];
+        environmentFiles = [
+            "${cfg.foundry.confRoot}/.env"
+        ];
+        ports = [ "30000:30000" ];
+      };
+    };
   };
 }
