0356f2d3bb3a8e5c213305c198ed24cdc6d47da1
Joe Fredette <jfredett@gmail.com>
2025-07-13 10:36:23 -0400

    k3s autocharts for nfs, vault

--- services/k3s/default.nix
+++ services/k3s/default.nix
@@ -8,42 +8,102 @@
   options.laurelin.services.k3s = with types; {
     enable = mkEnableOption "Enable k3s";
     role = mkOption {
-      type = "string";
       default = "server";
       description = ''
         set k3s role
       '';
+    };
+    nfs = {
+      enable = mkEnableOption "Enable NFS";
+      host = mkOption {
+        description = ''
+          set nfs server to attach to the nfs driver
+        '';
+      };
+    };
+    vault-operator = {
+      enable = mkEnableOption "Enable Vault Operator";
     };
   };
 
   config = let
     cfg = config.laurelin.services.k3s;
   in mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [
-      6443 # required so that pods can reach the API server (running on port 6443 by default)
-      2379 # etcd clients: required if using a "High Availability Embedded etcd" configuration
-      2380 # etcd peers: required if using a "High Availability Embedded etcd" configuration
-    ];
-    networking.firewall.allowedUDPPorts = [
-      8472 # flannel: required if using multi-node for inter-node networking
-    ];
-    services.k3s.enable = true;
-    services.k3s.role = "server";
-    services.k3s.configPath = narya.infra.k3s.config;
 
-    services.k3s.extraFlags = toString [
-      "--debug" # Optionally add additional args to k3s
-    ];
+      environment.systemPackages = if cfg.nfs.enable then [ pkgs.nfs-utils ] else [];
+      boot.supportedFilesystems = if cfg.nfs.enable then [ "nfs" ] else [];
 
-    environment.etc."rancher/k3s/registries.yaml".text = /* yaml */ ''
-      mirrors:
-        docker-registry.emerald.city:
-          endpoint:
-            - "https://docker-registry.emerald.city/v2"
-      configs:
-        "docker-registry.emerald.city":
-          tls:
-            insecure_skip_verify: true
-      '';
-  };
+      networking.firewall = {
+        allowedTCPPorts = [
+          6443 # required so that pods can reach the API server (running on port 6443 by default)
+          2379 # etcd clients: required if using a "High Availability Embedded etcd" configuration
+          2380 # etcd peers: required if using a "High Availability Embedded etcd" configuration
+        ];
+
+        allowedUDPPorts = [
+          8472 # flannel: required if using multi-node for inter-node networking
+        ];
+      };
+
+      services = {
+        rpcbind.enable = lib.mkForce cfg.nfs.enable;
+        k3s = {
+          enable = true;
+          role = "server";
+          configPath = narya.infra.k3s.config;
+          extraFlags = toString [
+            "--debug" # Optionally add additional args to k3s
+          ];
+
+          autoDeployCharts = let
+              cert_manager_repo = "https://charts.jetstack.io";
+              vault_operator_package = "https://github.com/hashicorp/vault-secrets-operator/archive/refs/tags/v0.10.0.tar.gz";
+              vault_package = "https://github.com/hashicorp/vault-helm/archive/refs/tags/v0.30.0.tar.gz";
+              nfs_provisioner_package = "https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner/releases/download/nfs-subdir-external-provisioner-4.0.18/nfs-subdir-external-provisioner-4.0.18.tgz";
+          in lib.mkIf (cfg.role == "server") {
+
+              vault = mkIf cfg.vault-operator.enable {
+                # repo = "https://helm.releases.hashicorp.com";
+                # name = "hashicorp/vault";
+                # version = "0.28.1";
+                package = pkgs.fetchurl {
+                  url = vault_package;
+                  hash = "sha256-Ea1F3b+CA8UqAS/Xdo4lD910NGOZbtc9r6vFd/VYIxE=";
+                };
+                createNamespace = true;
+                targetNamespace = "kube-infra";
+              };
+
+              nfs-storage = mkIf cfg.nfs.enable {
+                  package = pkgs.fetchurl {
+                    url = nfs_provisioner_package;
+                    hash = "sha256-qEIWPB9e7cSJPh9oXkJJsN3WJTPDyUEijuaglqwwj28=";
+                  };
+                  targetNamespace = "kube-infra";
+                  createNamespace = true;
+                  values = {
+                    "nfs" = {
+                      "server" = cfg.nfs.host;
+                      "path" = "/volume1/k8s";
+                    };
+                    "storageClass" = {
+                      "name" = "nancy-nfs";
+                    };
+                  };
+                };
+            };
+        };
+      };
+
+      environment.etc."rancher/k3s/registries.yaml".text = /* yaml */ ''
+        mirrors:
+          docker-registry.emerald.city:
+            endpoint:
+              - "https://docker-registry.emerald.city/v2"
+        configs:
+          "docker-registry.emerald.city":
+            tls:
+              insecure_skip_verify: true
+        '';
+    };
 }
